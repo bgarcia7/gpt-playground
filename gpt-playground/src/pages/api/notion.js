c53620cd1400068322277e20ccd802e655479498
brandon <bcgarcia3@gmail.com>
2023-04-06 15:29:02 -0400

    End to end v1 up

--- gpt-playground/src/pages/api/notion.js
+++ gpt-playground/src/pages/api/notion.js
@@ -1,4 +1,12 @@
 const { Client } = require("@notionhq/client")
+const { Configuration, OpenAIApi } = require("openai");
+require('dotenv').config();
+const configuration = new Configuration({
+  organization: 'org-JoyTBJ8CFCC0eUSQnlLSHDxK',
+  apiKey: process.env.OPENAI_API_KEY
+});
+const openai = new OpenAIApi(configuration);
+
 
 /*
 curl -X POST 'http://localhost:3000/api/notion' -d '{
@@ -10,6 +18,29 @@ curl -X POST 'http://localhost:3000/api/notion' -d '{
 
 // TODO: test for missing inputs
 // TODO: test for incorrect inputs for all 3 inputs
+
+function format_todo(notion_db_id, todoItem){
+  console.log(todoItem)
+  return {
+    parent: {
+      database_id: notion_db_id,
+    },
+    properties: {
+      'title': {
+        type: 'title',
+        title: [
+          {
+            type: 'text',
+            text: {
+              content: todoItem['toDoItem'],
+            },
+          },
+        ],
+      }
+    },
+  }
+}
+
 
 export default async function handler(req, res) {
   try {
@@ -25,25 +56,23 @@ export default async function handler(req, res) {
       auth: notion_token,
     })
 
-    const response = await notion.pages.create({
-      parent: {
-        database_id: notion_db_id,
-      },
-      properties: {
-        'Name': {
-          type: 'title',
-          title: [
-            {
-              type: 'text',
-              text: {
-                content: text,
-              },
-            },
-          ],
-        }
-      },
+    // Get ToDos from OpenAI
+    let userMessage = `create one or more tasks for the following request: ${text}
+    Only return a well structured JSON array: [{"toDoItem":"todo item"}]`
+  
+    console.log('OPEN AI KEY', process.env.OPENAI_API_KEY)
+    const completion = await openai.createChatCompletion({
+      model: "gpt-4",
+      messages: [{role: "system", content: "You are an executive assistant helping a user create todo tasks. "}, {role: "user", content: userMessage}],
     });
-    console.log(response);
+
+    let todos = JSON.parse(completion.data.choices[0].message.content.replace('\n',''))
+    console.log('FORMATTED TODOS:', todos)
+
+    const response = await Promise.all(
+      todos.map(todo => notion.pages.create(format_todo(notion_db_id, todo)))
+    ) 
+
     res.status(200).json({ 'status': '200', 'data': 'todo item uploaded' })
   } catch (e) {
     // TODO: handle errors better than this
